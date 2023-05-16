6e5ae15bab4ec3c137098dbacb83fbc73116f593
Douglas Miranda <douglas.miranda250296@gmail.com>
2023-05-16 17:02:42 -0300

    :bricks: Criando cabeçalho da página.

---
+++ src/componentes/Cabecalho/index.jsx
@@ -0,0 +1,13 @@
+export default function Cabecalho(){
+    return(
+        <header>
+            <img src="" alt="Logo do Alura Space"/>
+            <div>
+                <input type="text" 
+                placeholder="O que você procura?"
+                />
+                <img src="" alt="Ícone de lupa" />
+            </div>
+        </header>
+    )
+}
